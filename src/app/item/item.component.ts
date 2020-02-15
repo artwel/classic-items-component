955d5b26aa6183fc8262ec948797e9ecb2beb44a
artwel <welsch.arthur@gmail.com>
2020-02-15 14:18:52 +0100

    created components to fetch item data from backend;

---
+++ src/app/item/item.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {Item, ItemsService} from '../../../generated-sources/openapi';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.scss']
+})
+export class ItemComponent implements OnInit {
+
+  constructor(private itemsService: ItemsService) { }
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  private loadData() {
+    this.itemsService.findItems().subscribe((items: Item[]) => {
+      console.log(items);
+    });
+  }
+
+}
